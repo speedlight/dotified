811a1e36cb2cb89864ead040fca5e58619d79122
Speedlight <tao.ceel@gmail.com>
2016-08-25 10:49:19 -0500

    usage opts in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,28 @@
-#!/bin/bash
+#!/bin/bash -e
 #
+#-----------------------------
+# Dotfiles install script
+# Carlos 'Speedlight' Egüez
+# Since 1999
+#-----------------------------
+
+# Show how to run the install script
+function show_usage() {
+    echo -e "Ready to be dotified?"
+    echo -e "Usage: $0 [arguments] \n"
+    echo -e "Arguments:"
+    echo "-h: Display this help message"
+    echo "-s: Silent mode: install default settings without prompting for input";
+    echo -e "-i: Interactive mode: confirm what to install. \n"
+    exit 0;
+}
+
+if [ $# -eq 0 ]; then
+    echo -e "You need an argument\n"
+    show_usage
+fi
+
+
 #------------
 # Variables
 #------------
@@ -7,17 +30,20 @@
 dir=$HOME/dotfiles
 bakdir=$HOME/dotfiles_bak
 configs="bashrc bash_aliases vimrc .vim Xdefaults"
-deps="git vim"
+deps="git bash"
 
 #------------
-# Funciones
+# Messages
 #------------
 
-warning() { echo "\033[1;32m=> $1\033[0m"; }
-error() { echo "\033[1;31m=> Error: $1\033[0m"; }
+#warning() { echo '\033[1;32m=> $1\033[0m;' }
+#error() { echo '\033[1;31m=> Error: $1\033[0m;' }
+#list_item() { echo '\033[1;32m✔\033[0m $1;' }
+#error_item() { echo '\033[1;31m✖\033[0m $1;' }
 
-list_item() { echo " \033[1;32m✔\033[0m $1"; }
-error_item() { echo " \033[1;31m✖\033[0m $1"; }
+#------------
+# Functions
+#------------
 
 backup() {
 	mkdir -p $bakdir
@@ -26,16 +52,51 @@ backup() {
 	done
 }
 
-deps() {
-	type -p $1 &> /dev/null
-	local instaled=$?
-		if [ $instaled -eq 0 ]; then
-			list_item $1
-		else
-			error_item $1
-		fi
-	return $instaled
+dependencies() {
+    echo -e "Verficando dependencias.."
+    not_met=0
+
+    for dep in $deps; do
+        if [ -x "$(command -v $dep)" ]; then
+            
+            echo "Dependencia $dep cumplida!"
+        else
+            not_met=1
+            nodeps="$dep"
+            echo "Dependencia $dep no cumplida!"
+            exit 1
+        fi
+    done
+    #	met=$?
+    #	not_met=$(echo "$not_met + $met" | bc)
+    #done
+    #
+    #if [ $not_met -gt 0 ]; then
+    #	error ""
+    #	exit 1
+    #fi
+#			list_item $1
+#		else
+#			error_item $1
+#		fi
+#	return $instaled
 }
+
+silent_install() {
+    echo -e "Ready to be dotified?"
+    echo -e "Silent dotified process... Let the robot do his job..."
+    sleep 1
+    dependencies
+}
+
+interactive_install() {
+    echo -e "Ready to be dotified?"
+    echo -e "Interactive dotified process... Better a human machine..."
+    sleep 1
+    dependencies
+}
+
+
 
 instalation() {
 	mkdir -p $dir
@@ -49,22 +110,20 @@ instalation() {
 #-------------
 
 # copiando archivos indicados en $archivos a $bakdir
-warning "Backup configs to: $dotfiles_bak.."
-backup
+#echo -e "Backup configs to: $dotfiles_bak.."
+# backup
 
-#aviso "Verficando dependencias.."
-#not_met=0
-#for need in "${dependencias[@]}"; do
-#	deps $need
-#	met=$?
-#	not_met=$(echo "$not_met + $met" | bc)
-#done
-#
-#if [ $not_met -gt 0 ]; then
-#	error "Dependencia $not_met no cumplida!"
-#	exit 1
-#fi
-warning "Instaling..."
+#echo -e "Instaling..."
 #instalation
 
-warning "Done!"
+#echo -e "Done!"
+
+while getopts 'hsi?' flag; do
+    case "${flag}" in
+        "h") show_usage; exit 0;;
+        "s") silent_install ;;
+        "i") interactive_install ;;
+        "?") show_usage; exit 0;;
+        *) echo -e "Unexpected option, see -h for details" ;;
+    esac
+done
