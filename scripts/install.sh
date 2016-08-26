535b2ecc3b093ed2c7aece63f5996c3cd2e375bd
Speedlight <tao.ceel@gmail.com>
2016-08-26 17:22:35 -0500

    backup of original files, fixes in options selection

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,30 +6,13 @@
 # Since 1999
 #-----------------------------
 
-# Show how to run the install script
-function show_usage() {
-    echo -e "Ready to be dotified?"
-    echo -e "Usage: $0 [arguments] \n"
-    echo -e "Arguments:"
-    echo "-h: Display this help message"
-    echo "-s: Silent mode: install default settings without prompting for input";
-    echo -e "-i: Interactive mode: confirm what to install. \n"
-    exit 0;
-}
-
-if [ $# -eq 0 ]; then
-    echo -e "You need an argument\n"
-    show_usage
-fi
-
-
 #------------
 # Variables
 #------------
 
-dir=$HOME/dotfiles
-bakdir=$HOME/dotfiles_bak
-configs="bashrc bash_aliases vimrc .vim Xdefaults"
+DOTSDIR=$HOME/dotfiles
+DOTSBAKDIR=$HOME/dotfiles_orig
+DOTS="bashrc bash_aliases vimrc vim Xdefaults config/terminator"
 deps="git bash"
 
 #------------
@@ -41,62 +24,114 @@ deps="git bash"
 #list_item() { echo '\033[1;32m✔\033[0m $1;' }
 #error_item() { echo '\033[1;31m✖\033[0m $1;' }
 
-#------------
-# Functions
-#------------
-
-backup() {
-	mkdir -p $bakdir
-	for file in $config; do
-	cp -Rf  $HOME/.$file $bakdir/.
-	done
+show_usage() {
+    echo -e "Ready to be dotified?"
+    echo -e "Usage: $0 [arguments] \n"
+    echo -e "Arguments:"
+    echo "-h: Display this help message"
+    echo "-s: Silent mode: install default settings without prompting for input";
+    echo -e "-i: Interactive mode: confirm what to install. \n"
+    exit 0;
 }
 
-dependencies() {
-    echo -e "Verficando dependencias.."
-    not_met=0
+while getopts ':hsi?' flag; do
+    case "${flag}" in
+        "h") show_usage; exit 0;;
+        "s") silent=true;;
+        "i") interactive=true ;;
+        "?") show_usage; exit 0;;
+        *) echo -e "Unexpected option -$OPTARG, see -h for details" >&2 ;;
+    esac
+done
 
-    for dep in $deps; do
-        if [ -x "$(command -v $dep)" ]; then
-            
-            echo "Dependencia $dep cumplida!"
-        else
-            not_met=1
-            nodeps="$dep"
-            echo "Dependencia $dep no cumplida!"
-            exit 1
-        fi
-    done
-    #	met=$?
-    #	not_met=$(echo "$not_met + $met" | bc)
-    #done
-    #
-    #if [ $not_met -gt 0 ]; then
-    #	error ""
-    #	exit 1
-    #fi
+if [ $# -eq 0 ]; then
+    echo -e "You need an argument\n"
+    exit 1
+elif [[ $silent ]] && [[ $interactive ]]; then
+    echo -e "\033[91mOptions -s and -i are mutually exclusive. Use one or the other.\033[m"
+    exit 1;
+fi
+
+echo -e "Ready to be dotified?"
+
+echo -e "Verficando dependencias.."
+not_met=0
+for dep in $deps; do
+    if [ -x "$(command -v $dep)" ]; then
+        echo "Dependencia $dep cumplida!"
+    else
+        not_met=1
+        nodeps="$dep"
+        echo "Dependencia $dep no cumplida!"
+        exit 1
+    fi
+done
+#	met=$?
+#	not_met=$(echo "$not_met + $met" | bc)
+#done
+#
+#if [ $not_met -gt 0 ]; then
+#	error ""
+#	exit 1
+#fi
 #			list_item $1
 #		else
 #			error_item $1
 #		fi
 #	return $instaled
+
+backup_orig() {
+    while true; do
+        read -e -n 1 -r -p "Do you want to backup your original dotfiles? [y/N] " choice
+        case $choice in
+            [yY])
+                while true; do
+                    if [ -d $DOTSBAKDIR ]; then
+                        read -e -n 1 -r -p "Backup directory $DOTSBAKDIR already exist, do you want to overwrite? [y/N]" resp
+                        case $resp in
+                            [yY])
+                                echo -e "Coping your dots to $DOTSBAKDIR, please wait..."
+                                for dot in $DOTS; do
+                                    cp -Rf $HOME/.$dot $DOTSBAKDIR/.
+                                done
+                                echo -e "Your originals configs has been backed up to "
+                                break;;
+                            [nN])
+                                echo -e "Instalation aborted, please copy $DOTSBAKDIR to other location"
+                                exit 1;
+                                break;;
+                            *)
+                                echo -e "PLease choose y or n.";;
+                        esac
+                    elif ! [ -d $DOTSBAKDIR ]; then
+                        mkdir -p $DOTSBAKDIR
+                        echo -e "Coping your dots to $DOTSBAKDIR, please wait..."
+                        for dot in $DOTS; do
+                            cp -Rf $HOME/.$dot $DOTSBAKDIR/.
+                        done
+                        echo -e "Your originals configs has been backed up to $DOTSBAKDIR"
+                        break
+                    fi
+                done
+                break;;
+            [nN])
+                echo -e "No configs backed up"
+                break;;
+            *)
+                echo -e "\033[91mPlease choose y or n.\033[m";;
+        esac
+    done
 }
 
 silent_install() {
-    echo -e "Ready to be dotified?"
     echo -e "Silent dotified process... Let the robot do his job..."
     sleep 1
-    dependencies
 }
 
 interactive_install() {
-    echo -e "Ready to be dotified?"
     echo -e "Interactive dotified process... Better a human machine..."
     sleep 1
-    dependencies
 }
-
-
 
 instalation() {
 	mkdir -p $dir
@@ -118,12 +153,11 @@ instalation() {
 
 #echo -e "Done!"
 
-while getopts 'hsi?' flag; do
-    case "${flag}" in
-        "h") show_usage; exit 0;;
-        "s") silent_install ;;
-        "i") interactive_install ;;
-        "?") show_usage; exit 0;;
-        *) echo -e "Unexpected option, see -h for details" ;;
-    esac
-done
+if [ $silent ]; then
+    silent_install
+fi
+
+if [[ $interactive ]] && ! [[ $silent ]]; then
+    backup_orig
+    interactive_install
+fi
