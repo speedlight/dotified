67f321a99a69f3833450987f8a7c2804a1d3a6bb
speedlight <tao.ceel@gmail.com>
2016-08-31 16:55:52 -0500

    first version of install script complete

--- scripts/install.sh
+++ scripts/install.sh
@@ -134,10 +134,21 @@ backup() {
     done
 }
 
+fonts() {
+    echo -e "Updating fonts cache, including $DOTSDIR/fonts location.."
+    if [ ! -d $HOME/.fonts ]; then
+        mkdir -p $HOME/.fonts
+    fi
+    ln -sf $DOTSDIR/fonts $HOME/.fonts/.
+    fc-cache -fv >/dev/null
+}
+
 silent_install() {
     echo -e "Silent dotified process... Let the robot do his job..."
     mkdir -p $DOTSBAKDIR
     bkpdots
+    sleep 1
+    fonts
     sleep 1
 }
 
@@ -165,4 +176,5 @@ fi
 if [[ $interactive ]] && ! [[ $silent ]]; then
     backup
     interactive_install
+    fonts
 fi
